d22c3589afffc304aa3d8dfd30afccfb1a0fce07
Brim Fox <helicalzero@gmail.com>
2018-06-19 19:56:01 -0700

    init

---
+++ spec/preview-flexible-survival-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PreviewFlexibleSurvivalView from '../lib/preview-flexible-survival-view';
+
+describe('PreviewFlexibleSurvivalView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
